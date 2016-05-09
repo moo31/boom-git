40a7205de1a91ddd5ff985d6254713d3d259de23
Kim Abe <kimla010@hotmail.com>
2016-05-09 16:09:43 -0400

    Initial Commit

---
+++ boom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  boom
+//
+//  Created by Kim Abe on 5/9/16.
+//  Copyright © 2016 Moo31. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
